3e479aafb5791a857b21eb1974a40f3ca072c3f4
Jai Sri Vidya Mokshagundam <srividyamokshagundam@gmail.com>
2026-06-24 12:28:19 +0530

    Create ArraysProject.java

---
+++ Arrays/ArraysProject.java
@@ -0,0 +1,51 @@
+import java.util.*;
+public class ArraysProject{
+    public static void marksOfStudents(int[] arr,int n ){
+        int max=arr[0],min=arr[0];
+        for(int i=0;i<n;i++){
+            if(arr[i]>max){
+                max=arr[i];
+            }
+            if(arr[i] < min)
+            {
+                min=arr[i];
+            }
+        }
+        System.out.println("Highest Mark: "+max);
+        System.out.println("Lowest Mark: "+min);
+    }
+    public static void avgOfMarks(int[] arr,int n){
+        int sum =0;
+        int avg;
+        for(int i=0;i<n;i++){
+            sum+=arr[i];
+        }
+        avg=sum/n;
+        System.out.println("Average of marks:"+avg);
+    }
+    public static int totalPassed(int[] arr,int n){
+        int count=0;
+        for(int i=0;i<n;i++){
+            if(arr[i]>35){
+                count++;
+            }
+        }
+        return count;
+    }
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n= sc.nextInt();
+        int[] arr = new int[n];
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+        }
+        System.out.print("Marks of students: ");
+        for(int i=0;i<n;i++){
+            System.out.print(arr[i]+" ");
+        }
+        marksOfStudents(arr,n);
+        avgOfMarks(arr,n);
+        System.out.println("No.of Students passed: "+totalPassed(arr,n));
+
+    }
+}
