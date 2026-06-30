3a84b2b8a88dcef637f19d170010ee5f87e5d119
Jai Sri Vidya Mokshagundam <srividyamokshagundam@gmail.com>
2026-06-30 21:01:04 +0530

    Update and rename ArraysProject.java to StudentGradeManagement.java

--- Arrays/StudentGradeManagement.java
+++ Arrays/StudentGradeManagement.java
@@ -1,5 +1,5 @@
 import java.util.*;
-public class ArraysProject{
+public class StudentGradeManagement{
     public static void marksOfStudents(int[] arr,int n ){
         int max=arr[0],min=arr[0];
         for(int i=0;i<n;i++){
